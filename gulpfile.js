202c08a6cea7de050009f91a97d8ab8507db5a4e
Matthew Dean <matthewdean.me@gmail.com>
2016-05-11 15:46:47 -0700

    For sanity - compiles Less's benchmark .less file

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,30 @@
 var gulp = require('gulp');
+var postcss = require('postcss');
+var less = require('./');
+var autoprefixer = require('autoprefixer');
+var fs = require('fs');
+var path = require('path');
 
 gulp.task('test', function() {
     var mocha = require('gulp-mocha');
     return gulp.src('test/*.js', { read: false }).pipe(mocha());
 });
 
-gulp.task('integration', function(done) {
-    var postcss = require('postcss');
-    var less = require('./');
-    var autoprefixer = require('autoprefixer');
-    var fs = require('fs');
-    var path = require('path');
+function testLess(filename, plugins) {
+    var testFile = fs.readFileSync(path.join(__dirname, 'test/less/' + filename + '.less'), { });
 
-    var testFile = fs.readFileSync(path.join(__dirname, 'test/less/tests.less'), { });
-
-    return postcss([
-		less(), autoprefixer()
-	]).process(testFile.toString(), { parser: less.parser, from: 'test/less/tests.less' }).then(function (result) {
-		console.log(result);
+    return postcss(plugins).process(testFile.toString(), { parser: less.parser, from: 'test/less/' + filename + '.less' }).then(function (result) {
+		console.log(result.css);
 	}, function(err) {
 		console.log(err.message);
 	});
+}
 
+gulp.task('integration', function(done) {
+	testLess('tests', [less(), autoprefixer()]);
+});
+
+
+gulp.task('benchmark', function(done) {
+    testLess('benchmark', [less()]);
 });
